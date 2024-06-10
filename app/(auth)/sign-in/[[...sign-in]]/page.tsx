2e4aa11c289c4ca1dec9eafaa51840eca2abc82c
McCowsky <makowskimateusz@outlook.com>
2024-06-10 08:44:20 +0200

    fixes

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,27 +1,27 @@
-import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 export default function Page() {
   return (
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-      <div className="h-full lg:flex flex-col items-center justify-center px-4">
-        <div className="text-center space-y-4 pt-16">
-          <h1 className="font-bold text-3xl text-[#2E2A47]"></h1>
-          <p className="text-base text-[#7E8CA0]">
+    <div className='grid min-h-screen grid-cols-1 lg:grid-cols-2'>
+      <div className='h-full flex-col items-center justify-center px-4 lg:flex'>
+        <div className='space-y-4 pt-16 text-center'>
+          <h1 className='text-3xl font-bold text-[#2E2A47]'></h1>
+          <p className='text-base text-[#7E8CA0]'>
             Log in or Create account to get back to your dashboard
           </p>
         </div>
-        <div className="flex items-center justify-center mt-8">
+        <div className='mt-8 flex items-center justify-center'>
           <ClerkLoaded>
-            <SignIn path="/sign-in" />
+            <SignIn path='/sign-in' />
           </ClerkLoaded>
           <ClerkLoading>
-            <Loader2 className="animate-spin text-muted-foreground" />
+            <Loader2 className='animate-spin text-muted-foreground' />
           </ClerkLoading>
         </div>
       </div>
-      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
-        <Image src="/logo.png" height={100} width={100} alt="WealthPal logo" />
+      <div className='hidden h-full items-center justify-center bg-blue-600 lg:flex'>
+        <Image src='/logo.png' height={100} width={100} alt='WealthPal logo' />
       </div>
     </div>
   );
